1e435cc060129b1c227604ab71c653bb17a9178a
Younes Belcaid <younes.belcaid@businessdecision.be>
2021-02-15 11:30:52 +0100

    forget an import

--- src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
@@ -2,6 +2,7 @@ package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
